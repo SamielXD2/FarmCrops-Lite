f8336854b3ada80bd337034bc556e0c9c4cb3078
SamielXD2 <samiellxd@gmail.com>
2026-02-15 09:23:18 +0800

    Add files via upload

---
+++ src/main/java/dev/samiel/farmcrops/commands/SellCommand.java
@@ -0,0 +1,22 @@
+package dev.samiel.farmcrops.commands;
+import org.bukkit.ChatColor;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+public class SellCommand implements CommandExecutor {
+    private final FarmCrops plugin;
+    public SellCommand(FarmCrops plugin) {
+        this.plugin = plugin;
+    }
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        if (!(sender instanceof Player)) {
+            sender.sendMessage("This command can only be run by a player.");
+            return true;
+        }
+        Player player = (Player) sender;
+        plugin.getSellGUI().openGUI(player);
+        return true;
+    }
+}
